e1fd6951f8332881c1a54b18f0bfcbc9f243bc23
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-28 08:34:22 +0100

    Created DTO, repo, exception, service and controller for the Films entity

---
+++ src/main/java/com/qa/rest/FilmsController.java
@@ -0,0 +1,49 @@
+package com.qa.rest;
+
+import com.qa.domain.Films;
+import com.qa.dto.FilmsDTO;
+import com.qa.service.FilmsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FilmsController {
+
+    private final FilmsService service;
+
+    @Autowired
+    public FilmsController(FilmsService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/createFilms")
+    public ResponseEntity<FilmsDTO> createFilms(@RequestBody Films films) {
+        return new ResponseEntity<FilmsDTO>(this.service.createFilms(films), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/getAllFilms")
+    public ResponseEntity<List<FilmsDTO>> getAllStandardScreens() {
+        return ResponseEntity.ok(this.service.readFilms());
+    }
+
+    @GetMapping("/getFilmsById/{filmsID}")
+    public ResponseEntity<FilmsDTO> getFilmsById(@PathVariable Long filmsID) {
+        return ResponseEntity.ok(this.service.getFilmsById(filmsID));
+    }
+
+    @PutMapping("/updateFilms/{filmsID}")
+    public ResponseEntity<FilmsDTO> updateFilms(@PathVariable Long filmsID, @RequestBody Films films){
+        return ResponseEntity.ok(this.service.updateFilms(filmsID, films));
+    }
+
+    @DeleteMapping("/deleteFilms/{filmsID}")
+    public ResponseEntity<?> deleteFilms(@PathVariable Long filmsID){
+        return this.service.deleteFilms(filmsID)
+                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+                : ResponseEntity.ok(this.service.getFilmsById(filmsID));
+    }
+}
