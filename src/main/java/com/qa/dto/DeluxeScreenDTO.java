490d9134a5172cb57f486936e982ea648946dc85
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-05-28 12:46:31 +0300

    finished deluxe dto class

--- src/main/java/com/qa/dto/DeluxeScreenDTO.java
+++ src/main/java/com/qa/dto/DeluxeScreenDTO.java
@@ -1,24 +1,28 @@
 package com.qa.dto;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DeluxeScreenDTO {
 
     private Long deluxeScreeningId;
-    private Long filmId;
-    private double screeningTime;
-    private String seatsBooked;
-//    private List<package_info> package_infoList;
+    private Long deluxeFilmId;
+    private Long deluxeBookingId;
+    private Double deluxeScreeningTime;
+    private String deluxeSeatsBooked;
+
+    private List<BookingInfoDTO> bookings;
 
     public DeluxeScreenDTO(){
 
     }
 
-    public DeluxeScreenDTO(Long filmId, double screeningTime, String seatsBooked) {
+    public DeluxeScreenDTO(Long deluxeFilmId, Long deluxeBookingId, Double deluxeScreeningTime, String deluxeSeatsBooked) {
         super();
-        this.filmId = filmId;
-        this.screeningTime = screeningTime;
-        this.seatsBooked = seatsBooked;
+        this.deluxeFilmId = deluxeFilmId;
+        this.deluxeBookingId = deluxeBookingId;
+        this.deluxeScreeningTime = deluxeScreeningTime;
+        this.deluxeSeatsBooked = deluxeSeatsBooked;
     }
 
     public Long getDeluxeScreeningId() {
@@ -29,35 +33,73 @@ public class DeluxeScreenDTO {
         this.deluxeScreeningId = deluxeScreeningId;
     }
 
-    public Long getFilmId() {
-        return filmId;
+    public Long getDeluxeFilmId() {
+        return deluxeFilmId;
     }
 
-    public void setFilmId(Long filmId) {
-        this.filmId = filmId;
+    public void setDeluxeFilmId(Long deluxeFilmId) {
+        this.deluxeFilmId = deluxeFilmId;
     }
 
-//    public List<package_info> getPackage_infoList() {
-//        return package_infoList;
-//    }
-
-    public double getScreeningTime() {
-        return screeningTime;
+    public Long getDeluxeBookingId() {
+        return deluxeBookingId;
     }
 
-    public void setScreeningTime(double screeningTime) {
-        this.screeningTime = screeningTime;
+    public void setDeluxeBookingId(Long deluxeBookingId) {
+        this.deluxeBookingId = deluxeBookingId;
     }
 
-    public String getSeatsBooked() {
-        return seatsBooked;
+    public Double getDeluxeScreeningTime() {
+        return deluxeScreeningTime;
     }
 
-    public void setSeatsBooked(String seatsBooked) {
-        this.seatsBooked = seatsBooked;
+    public void setDeluxeScreeningTime(Double deluxeScreeningTime) {
+        this.deluxeScreeningTime = deluxeScreeningTime;
     }
 
-//    public void setPackage_infoList(List<package_info> package_infoList) {
-//        this.package_infoList = package_infoList;
-//    }
+    public String getDeluxeSeatsBooked() {
+        return deluxeSeatsBooked;
+    }
+
+    public void setDeluxeSeatsBooked(String deluxeSeatsBooked) {
+        this.deluxeSeatsBooked = deluxeSeatsBooked;
+    }
+
+    public List<BookingInfoDTO> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<BookingInfoDTO> bookings) {
+        this.bookings = bookings;
+    }
+
+    @Override
+    public String toString(){
+        return "DeluxeScreenDTO{" +
+                "deluxeScreeningId=" + deluxeScreeningId +
+                ", deluxeFilmId=" + deluxeFilmId +
+                ", deluxeBookingId=" + deluxeBookingId +
+                ", deluxeScreeningTime=" + deluxeScreeningTime +
+                ", deluxeSeatsBooked=" + deluxeSeatsBooked + '\'' +
+                ", bookings=" + bookings +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeluxeScreenDTO that = (DeluxeScreenDTO) o;
+        return Objects.equals(deluxeScreeningId, that.deluxeScreeningId) &&
+                Objects.equals(deluxeFilmId, that.deluxeFilmId) &&
+                Objects.equals(deluxeBookingId, that.deluxeBookingId) &&
+                Objects.equals(deluxeScreeningTime, that.deluxeScreeningTime) &&
+                Objects.equals(deluxeSeatsBooked, that.deluxeSeatsBooked) &&
+                Objects.equals(bookings, that.bookings);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(deluxeScreeningId, deluxeFilmId, deluxeBookingId, deluxeScreeningTime, deluxeSeatsBooked, bookings);
+    }
 }
