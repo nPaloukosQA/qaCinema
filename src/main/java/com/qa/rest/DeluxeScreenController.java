e079b43dbc2e080a7bcec60c22ab3887efd5fbb5
nPaloukosQA <62283168+nPaloukosQA@users.noreply.github.com>
2020-05-28 13:47:20 +0300

    codded DeluxeScreenController

--- src/main/java/com/qa/rest/DeluxeScreenController.java
+++ src/main/java/com/qa/rest/DeluxeScreenController.java
@@ -1,4 +1,50 @@
-package com.qa.rest;
-
-public class DeluxeScreenController {
-}
+//package com.qa.rest;
+//
+//import com.qa.domain.DeluxeScreen;
+//import com.qa.dto.DeluxeScreenDTO;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//
+//import java.util.List;
+//
+//@RestController
+//public class DeluxeScreenController {
+//
+//    private final DeluxeScreenController service;
+//
+//    @Autowired
+//    public DeluxeScreenController(DeluxeScreenController service) {
+//        this.service = service;
+//    }
+//
+//    @PostMapping("/createDeluxeScreen")
+//    public ResponseEntity<DeluxeScreenDTO> createDeluxeScreen(@RequestBody DeluxeScreen deluxeScreen) {
+//        return new ResponseEntity<DeluxeScreenDTO>(this.service.createDeluxeScreen(deluxeScreen), HttpStatus.CREATED);
+//    }
+//
+//    @GetMapping("/getAllDeluxeScreens")
+//    public ResponseEntity<List<DeluxeScreenDTO>> getAllDeluxeScreens() {
+//        return ResponseEntity.ok(this.service.readDeluxeScreens());
+//    }
+//
+//    @GetMapping("/getDeluxeScreenById/{deluxeScreenId}")
+//    public ResponseEntity<DeluxeScreenDTO> getDeluxeScreenById(@PathVariable Long deluxeScreenId) {
+//        return ResponseEntity.ok(this.service.getDeluxeScreenById(deluxeScreenId));
+//    }
+//
+//    @PostMapping("/updateDeluxeScreen/{deluxeScreenId}")
+//    public ResponseEntity<DeluxeScreenDTO> updateDeluxeScreen(@PathVariable Long deluxeScreenId, @RequestBody DeluxeScreen deluxeScreen) {
+//        return ResponseEntity.ok(this.service.updateDeluxeScreen(deluxeScreenId, deluxeScreen));
+//    }
+//
+//    @DeleteMapping("/deleteDeluxeScreen/{deluxeScreenId}")
+//    public ResponseEntity<?> deleteDeluxeScreen(@PathVariable Long deluxeScreenId) {
+//        return this.service.deleteDeluxeScreen(deluxeScreenId)
+//                ? ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
+//                : ResponseEntity.ok(this.service.getDeluxeScreenById(deluxeScreenId));
+//    }
+//
+//}
