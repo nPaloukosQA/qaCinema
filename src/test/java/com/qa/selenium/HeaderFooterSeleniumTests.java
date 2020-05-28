4bc9d448b68640b6171df2a4a0bb3a41ab716634
ConwayQA <lukeconwayqa@googlemail.com>
2020-05-28 17:24:57 +0100

    Added more dependancies for selenium

--- src/test/java/com/qa/selenium/HeaderFooterSeleniumTests.java
+++ src/test/java/com/qa/selenium/HeaderFooterSeleniumTests.java
@@ -1,5 +1,61 @@
 package com.qa.selenium;
 
+import org.testng.ITestResult;
+import org.testng.annotations.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static java.lang.Thread.*;
+
+import java.io.File;
+
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class HeaderFooterSeleniumTests {
     
+    WebDriver driver;
+    ExtentReports report;
+    ExtentTest test;
+
+    @BeforeTest
+    public void startReport() {
+        report = new ExtentReports(System.getProperty("user.dir" + "/test-output/Report.html"), true);
+        report.addSystemInfo("Host Name", "QA").addSystemInfo("Tester", "Luke");
+        report.loadConfig(new File(System.getProperty("user.dir" + "\\extent-report.xml")));
+    }
+    
+    @BeforeMethod
+    public void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void seleniumHeaderTest() throws InterruptedException {
+        test = report.startTest("Testing Webpage Header");
+        driver.manage().window().maximize();
+        test.log(LogStatus.INFO, "Started chrome browser and made it fullscreen");
+        driver.get("http://127.0.0.1:5500/qaCinema/src/main/resources/static/homepage.html");
+        sleep(2000);
+    }
+    
+    @AfterMethod
+    public void getResult(ITestResult result){
+        driver.close();
+        if(result.getStatus() == ITestResult.FAILURE){
+            test.log(LogStatus.FAIL, "Test has failed " + result.getName());
+            test.log(LogStatus.FAIL, "Test has failed " + result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.log(LogStatus.PASS, "Test has passed " + result.getName());
+        }
+        report.endTest(test);
+    }
+
+    
+    @AfterTest
+    public void endReport(){
+        report.flush();
+        report.close();
+    }
 }
