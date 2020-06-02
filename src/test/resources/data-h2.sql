4bb7deeb5ca4b7cbb83075b3f447dc2dffbbb6b5
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-06-02 11:34:06 +0100

    fixed the integration tests

---
+++ src/test/resources/data-h2.sql
@@ -0,0 +1,6 @@
+INSERT INTO films (filmsid, films_classification, films_is_feature, filmsomdbid, films_title, films_currently_released) VALUES
+    (1, '12A', 1, 'tt4154756', 'Avengers: Infinity War', 1),
+    (2, '15', 1, 'tt1431045', 'Deadpool', 1),
+    (3, 'U', 1, 'tt0080684', 'Star Wars: Episode V - The Empire Strikes Back', 1),
+    (4, '12A', 0, 'tt3498820', 'Captain America: Civil War', 1),
+    (5, '12A', 1, 'tt4154796', 'Avengers: Endgame', 1);
