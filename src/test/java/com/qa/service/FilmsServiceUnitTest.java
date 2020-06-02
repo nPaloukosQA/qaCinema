b99e1d5efefe3fec03f638e0992de1942399e6a2
ConwayQA <lukeconwayqa@googlemail.com>
2020-06-02 16:54:39 +0100

    Spring application fixes

--- src/test/java/com/qa/service/FilmsServiceUnitTest.java
+++ src/test/java/com/qa/service/FilmsServiceUnitTest.java
@@ -109,13 +109,6 @@ public class FilmsServiceUnitTest {
         verify(repo, times(1)).findById(testID);
     }
 
-    @Test
-    public void findFilmsByTitleTest() {
-        when(this.repo.findFilmsByTitle(testFilmsWithID.getFilmsTitle())).thenReturn(java.util.Optional.ofNullable(testFilmsWithID));
-        when(this.mapper.map(testFilmsWithID, FilmsDTO.class)).thenReturn(filmsDTO);
-        assertEquals(this.service.getFilmsByTitle(this.testFilmsTitle), filmsDTO);
-        verify(repo, times(1)).findFilmsByTitle(testFilmsTitle);
-    }
 
     @Test
     public void getFilmsStandardScreeningsTest() {
