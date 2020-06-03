3acd3bf016d1e7531a9d75029815cb76a90a36a0
DanReedQA <dan@reed-wayside.co.uk>
2020-06-03 16:06:39 +0100

    added films list to DTo's

--- src/test/java/com/qa/selenium/OpeningTimesSeleniumTest.java
+++ src/test/java/com/qa/selenium/OpeningTimesSeleniumTest.java
@@ -7,9 +7,14 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertFalse;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -22,7 +27,8 @@ public class OpeningTimesSeleniumTest {
 
     @Before
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\dan\\intellij-workspace\\qaCinema\\chromedriver.exe");
         ChromeOptions opts = new ChromeOptions();
         //opts.setHeadless(true);
         this.driver = new ChromeDriver(opts);
@@ -37,6 +43,11 @@ public class OpeningTimesSeleniumTest {
     public void testOpeningTimesPage() {
         driver.manage().window().maximize();
         driver.get("http://localhost:" + port);
+        OpeningTimesSeleniumElements openingTimes = PageFactory.initElements(driver, OpeningTimesSeleniumElements.class);
+
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.textToBePresentInElement(openingTimes.getOpeningTimesTitle(), "Opening Times"));
+        assertFalse(openingTimes.getOpeningTimesTitle().getText().isEmpty());
     }
 
 
